c36d148ab86ca1de16ddcef48cdb604dc03c5e08
tlpackus <tlpackus@gmail.com>
2021-01-28 13:39:51 -0800

    I added forms to the html for subtraction, multiplications and division then added the nessesary user interface logic to the scripts.js because I already had the buisness logic

--- JS/scripts.js
+++ JS/scripts.js
@@ -17,9 +17,30 @@ const divide = function(number1, number2) {
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
+    let number1 = parseInt($("#add1").val());
+    let number2 = parseInt($("#add2").val());
+    let result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#sub1").val());
+    let number2 = parseInt($("#sub2").val());
+    let result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#mlt1").val());
+    let number2 = parseInt($("#mlt2").val());
+    let result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#div1").val());
+    let number2 = parseInt($("#div2").val());
+    let result = divide(number1, number2);
     $("#output").text(result);
   });
 });
